7d2850e977c8d73e9b774c7b52a78fa531f94882
smme2009 <smme2009@gmail.com>
2025-01-18 16:29:17 +0800

    feat: 新增Body參數接收的機制

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import Express from "express";
 import Env from "dotenv";
+import BodyParser from "body-parser";
 
 // 初始化env
 Env.config();
 
 const app = Express();
 const port = 3000;
+
+// 接收Body參數設定
+app.use(
+    BodyParser.urlencoded({
+        extended: true,
+    })
+);
 
 app.listen(port);
 
