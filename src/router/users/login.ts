e8ec3b173dddc190f0e94b1f0c7fd714cbc8ff25
smme2009 <smme2009@gmail.com>
2025-01-19 14:37:49 +0800

    feat: 新增登入API的文件

--- src/router/users/login.ts
+++ src/router/users/login.ts
@@ -7,7 +7,69 @@ const router: Router = Router();
 // 帳號Controller
 const ctrlAccount: CtrlUsers = new CtrlUsers();
 
-// 登入
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: 登入
+ *     tags: [使用者]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "joi@test.com"
+ *               password:
+ *                 type: string
+ *                 example: "joipass777"
+ *     responses:
+ *       200:
+ *         description: 成功登入
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "成功登入"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     jwtToken:
+ *                       type: string
+ *                       example: "xxx.xxx.xxx"
+ *
+ *       400:
+ *         description: 欄位格式錯誤
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "欄位格式錯誤"
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   example: ["E-Mail欄位必填", "密碼欄位必填"]
+ *       401:
+ *         description: 帳號或密碼錯誤
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "帳號或密碼錯誤"
+ */
 router.post("/login", (request: Request, response: Response) => {
     ctrlAccount.login(request, response);
 });
